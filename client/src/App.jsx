24d397ce33413236677035074dad233f690917ba
Thomas <tfnielson.se@gmail.com>
2023-03-06 15:33:21 -0700

    established api connection between front and back end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
+import Home from './components/Home'
+import Login from './components/Login'
+import _ from 'lodash'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+  console.log(user)
 
+  useEffect(() => {
+    fetch("http://127.0.0.1:3000/me")
+    .then((response) => {
+      if (response.ok) {
+        response.json().then((user) => setUser(user));
+        console.log(user.id)
+      }
+    });
+  }, []);
+
+
+  if (!user) return  <Login onLogin={setUser} />;
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>ERP</h1>
     </div>
   )
 }
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return(
+    <div>
+      <h1>ERP System</h1>
+    </div>
+  )
+}
+
+export default Home;
